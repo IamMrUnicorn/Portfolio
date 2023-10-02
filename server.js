72d1a382eca8160203a1134e6510921c26711a63
Ubuntu <ubuntu@ip-172-31-9-40.us-east-2.compute.internal>
2023-10-02 18:26:25 +0000

    production changes

--- server.js
+++ server.js
@@ -1,19 +1,44 @@
 import express from 'express';
 import path from 'path';
 import url from 'url';
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
+app.post('/send-email', async (req, res) => {
+  const { email, subject, text } = req.body;
+
+  let transporter = nodemailer.createTransport({
+	  service: 'gmail',
+	  auth: {
+	    user: process.env.EMAIL,
+	    pass: process.env.PASS
+	  }
+  });
+		  
+  let info = await transporter.sendMail({
+    from: email,
+    to: 'quintanajason4004@gmail.com',
+    subject,
+    text,
+  });
+
+  res.send('Email sent');
+});
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}/`);
 });
--- src/EmailMe.tsx
+++ src/EmailMe.tsx
@@ -1,21 +1,75 @@
-export const EmailMe = () => {
 
+import axios from 'axios'
+import { ChangeEvent, FormEvent, useState } from 'react';
+
+
+export const EmailMe = () => {
+  
+  interface EmailDataI {
+    name: string;
+    email: string;
+    message: string;
+  }
+  const [EmailData, setEmailData] = useState<EmailDataI>({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setEmailData({ ...EmailData, [name]: value });
+  };
+
+
+const handleSubmit = async (e: FormEvent) => {
+  e.preventDefault();
+  
+
+  if (!EmailData.name || !EmailData.email || !EmailData.message) {
+    alert('All fields are required.');
+    return;
+  }
+
+
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  if (!emailPattern.test(EmailData.email)) {
+    alert('Invalid email address.');
+    return;
+  }
+  
+
+  try {
+    const response = await axios.post('/send-email', {
+      email: EmailData.email,
+      subject: `Message from ${EmailData.name}`,
+      text: EmailData.message,
+    });
+    if (response.data === 'Email sent') {
+      alert('Email sent successfully');
+    }
+  } catch (error) {
+    alert('Failed to send email');
+    console.log(error)
+  }
+};
+ 
   return (
     <div className='bg-accent p-4 rounded-lg '>
-      <form className='flex flex-col justify-center '>
+      <form className='flex flex-col justify-center' onSubmit={handleSubmit}>
         <label className="flex flex-col text-accent-content fontB p-1">
           Enter your name
-          <input className="text-secondary-content rounded-lg p-2 bg-secondary" placeholder="doug" />
+          <input className="text-secondary-content rounded-lg p-2 bg-secondary" name='name' value={EmailData.name} onChange={handleChange} placeholder="doug" />
         </label>
         <label className="flex flex-col text-accent-content fontB p-1" >
           Enter your email
-          <input className="text-secondary-content rounded-lg p-2 bg-secondary" placeholder="dougiscool@gmail.com" />
+          <input className="text-secondary-content rounded-lg p-2 bg-secondary" name='email' value={EmailData.email} onChange={handleChange} placeholder="dougiscool@gmail.com" />
         </label>
         <label className="flex flex-col text-accent-content fontB p-1">
           Enter a cool message
-          <textarea cols={60} rows={10} className="text-secondary-content rounded-lg p-2 bg-secondary" placeholder="awesome projects, do you think i can borrow your BMO some time? :)" />
+          <textarea cols={60} rows={10} className="text-secondary-content rounded-lg p-2 bg-secondary" name='message' value={EmailData.message} onChange={handleChange} placeholder="awesome projects, do you think i can borrow your BMO some time? :)" />
         </label>
-        <button className="btn btn-primary fontC text-2xl w-min mx-auto ">send</button>
+        <button className="btn btn-primary fontC text-2xl w-min mx-auto " type='submit'>send</button>
       </form>
     </div>
   )
